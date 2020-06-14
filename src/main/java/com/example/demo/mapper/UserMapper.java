9dce21638508c667dacf30b284dffc09e8476d79
天使の羽 <45993782+805659172@users.noreply.github.com>
2020-06-14 23:42:33 +0800

    增加Component注解

--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -2,6 +2,7 @@ package com.example.demo.mapper;
 
 import com.example.demo.entity.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
@@ -10,6 +11,7 @@ import java.util.List;
  * User Mapper
  */
 @Mapper
+@Component
 public interface UserMapper {
     public List<User> getList();
 }
